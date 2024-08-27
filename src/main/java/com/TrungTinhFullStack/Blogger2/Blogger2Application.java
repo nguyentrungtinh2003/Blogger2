6d221bf61f6e20f5a02ac06ab33f0a2b3a86395d
nguyentrungtinh <trungtinhn300@gmail.com>
2024-08-27 20:29:49 +0700

    Tao du an Blog backend

---
+++ src/main/java/com/TrungTinhFullStack/Blogger2/Blogger2Application.java
@@ -0,0 +1,13 @@
+package com.TrungTinhFullStack.Blogger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blogger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blogger2Application.class, args);
+	}
+
+}
