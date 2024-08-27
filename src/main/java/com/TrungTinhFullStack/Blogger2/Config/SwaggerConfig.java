3a079d78f6bcf7f0d8f11c0530a567204e54ed01
nguyentrungtinh <trungtinhn300@gmail.com>
2024-08-27 21:12:41 +0700

    Tao package Config va thiet lap cac file cau hinh cors,web,swagger

---
+++ src/main/java/com/TrungTinhFullStack/Blogger2/Config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.TrungTinhFullStack.Blogger2.Config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicAPI() {
+
+        return GroupedOpenApi.builder()
+                .group("spring-public")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
